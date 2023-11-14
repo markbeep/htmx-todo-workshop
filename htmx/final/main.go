bc5dcb9e32af70c6db704bfec5708ddab2645430
Mark Csurgay <mcsurgay@student.ethz.ch>
2023-11-14 17:08:25 +0100

    made main simpler

--- htmx/final/main.go
+++ htmx/final/main.go
@@ -9,8 +9,6 @@ import (
 	"net/http"
 	"slices"
 	"strconv"
-	"strings"
-	"sync"
 
 	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
@@ -24,60 +22,44 @@ var (
 )
 
 var (
-	todoCounter = 0
-	lock        sync.Mutex
+	todoCounter = 0 // used for assigning todo ids
+	todos       = []internal.Todo{}
 )
 
 func main() {
 	flag.Parse() // parses command line flags defined above
 
 	r := chi.NewRouter()
-	r.Use(middleware.RequestID) // request IDs to distinguish different sessions
-	r.Use(middleware.Logger)    // adds helpful logging
-
-	todos := map[string][]internal.Todo{}
+	r.Use(middleware.Logger) // adds helpful logging
 
 	// Handles all GET requests to /
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		requestId := extractRequestUser(middleware.GetReqID(r.Context()))
-		userTodos, ok := todos[extractRequestUser(requestId)]
-		if !ok {
-			templ.Handler(components.Index([]internal.Todo{})).ServeHTTP(w, r)
-		} else {
-			templ.Handler(components.Index(userTodos)).ServeHTTP(w, r)
-		}
+		templ.Handler(components.Index(todos)).ServeHTTP(w, r)
 	})
 
 	// Handles all POST requests to /todo
 	r.Post("/todo", func(w http.ResponseWriter, r *http.Request) {
-		requestId := extractRequestUser(middleware.GetReqID(r.Context()))
-
 		r.ParseForm() // REQUIRED for the r.PostFormValue to get filled with values
 		text := r.PostFormValue("text")
 
-		lock.Lock()         // lock so we can safely increase todoCounter
-		defer lock.Unlock() // gets called even on panic, but only at the end of the function
-		todos[requestId] = append(todos[requestId], internal.Todo{Text: text, ID: todoCounter})
+		todos = append(todos, internal.Todo{Text: text, ID: todoCounter})
 		todoCounter++
 
-		templ.Handler(components.TodoList(todos[requestId])).ServeHTTP(w, r)
+		templ.Handler(components.TodoList(todos)).ServeHTTP(w, r)
 	})
 
 	// Handles all DELETE requests to /todo/{id}
 	r.Delete("/todo/{id:\\d+}", func(w http.ResponseWriter, r *http.Request) {
-		requestId := extractRequestUser(middleware.GetReqID(r.Context()))
-
 		// make sure the argument is a valid int (not too large for example)
 		id, err := strconv.Atoi(chi.URLParam(r, "id"))
 		if err != nil {
 			http.Error(w, "invalid id", http.StatusBadRequest)
 			return
 		}
-
-		todos[requestId] = slices.DeleteFunc[[]internal.Todo](todos[requestId], func(t internal.Todo) bool {
+		todos = slices.DeleteFunc[[]internal.Todo](todos, func(t internal.Todo) bool {
 			return t.ID == id
 		})
-		templ.Handler(components.TodoList(todos[requestId])).ServeHTTP(w, r)
+		templ.Handler(components.TodoList(todos)).ServeHTTP(w, r)
 	})
 
 	// starts listening
@@ -86,11 +68,4 @@ func main() {
 	if err := http.ListenAndServe(fullHost, r); err != nil {
 		log.Fatal(err)
 	}
-}
-
-// Takes the Request ID string from chi and cuts away the count at the end
-func extractRequestUser(requestId string) string {
-	// requestId is of format: Name/randomKey-00000x
-	splitted := strings.Split(requestId, "-")
-	return strings.Join(splitted[:len(splitted)-1], "")
 }
